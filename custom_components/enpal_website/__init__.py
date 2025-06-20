fc7d0289e28a4e00b31c3796943393171fe2176c
Robin Wessel <robinwessel1@gmail.com>
2025-06-20 11:19:45 +0200

    Schnittstellen Anpassungen

--- custom_components/enpal_website/__init__.py
+++ custom_components/enpal_website/__init__.py
@@ -1,17 +1,19 @@
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from .const import DOMAIN
 
+PLATFORMS = ["sensor"]
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    # Plattformen asynchron weiterleiten
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    # Plattformen asynchron entladen
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return unload_ok

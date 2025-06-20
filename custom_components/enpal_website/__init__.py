d20a1a586df357ce15e190d0c7f76f7264967b2a
Robin Wessel <robinwessel1@gmail.com>
2025-06-20 11:16:44 +0200

    Revert "Update __init__.py"
    This reverts commit 0a66a2ab2c34931f68378e8f8eab4d4ff74a8639.

--- custom_components/enpal_website/__init__.py
+++ custom_components/enpal_website/__init__.py
@@ -8,9 +8,9 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-   
-    await hass.config_entries.async_forward_entry_setups(entry, "sensor")
-    
+    hass.async_create_task(
+        await hass.config_entries.async_forward_entry_setups(entry, "sensor")
+    )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
